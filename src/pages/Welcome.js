0f213b6bc516c42b4357b4430f76e7c27d611b95
Marianna Zurek <zurekmarianna@gmail.com>
2023-04-17 18:56:45 +0200

    first commit

---
+++ src/pages/Welcome.js
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import {
+  MainLayout,
+  MainTitle,
+  Button,
+  Title,
+} from "../utils/styledComponents";
+
+const Welcome = () => {
+  const navigate = useNavigate();
+
+  const onStart = () => {
+    navigate("/dishes");
+  };
+  return (
+    <MainLayout style={{ margin: "0", paddingLeft: "80px" }}>
+      <MainTitle style={{ marginTop: "100px" }}>Hello!</MainTitle>
+      <MainTitle>Click the button below to see the list of carts:</MainTitle>
+      <Button
+        onClick={onStart}
+        style={{ fontSize: "25px", padding: "10px", marginTop: "35px" }}
+      >
+        <Title>All carts</Title>
+        <span
+          class="fas fa-angle-double-right"
+          style={{ fontSize: "23px" }}
+        ></span>
+      </Button>
+    </MainLayout>
+  );
+};
+
+export default Welcome;
---
+++ src/utils/styledComponents.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+  margin-left: 148px;
+  padding: 55px 55px 0px 55px;
+  overflow-x: hidden;
+`;
+
+export const MainTitle = styled.p`
+  font-size: 50px;
+  color: #2c2c31;
+  margin-top: 0;
+  margin-bottom: 20px;
+  width: 562px;
+`;
+
+export const Title = styled.p`
+  font-size: 15px;
+  margin: 0;
+  font-weight: bold;
+`;
+
+export const Button = styled.button`
+  color: #fff;
+  background-color: #2c2c31;
+  cursor: pointer;
+  border: 1px solid #2c2c31;
+  border-radius: 6px;
+  padding: 5px;
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+`;
